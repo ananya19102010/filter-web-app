4e93d7920d22d9fbc8674c06c81ce7c23104c635
ananya19102010 <73178449+ananya19102010@users.noreply.github.com>
2021-10-29 19:45:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+noseX=0;
+noseY=0;
 
+function preload(){
+lipstick=loadImage('https://i.postimg.cc/nMbNRDv9/l1.png');
 }
 
 function setup(){
@@ -21,13 +24,16 @@ function gotPoses(results){
     if(results.length>0)
     {
      console.log(results);
-     console.log("nose x="+results[0].pose.nose.x);
-     console.log("nose y="+results[0].pose.nose.y);
+     noseX=results[0].pose.nose.x-25;
+     noseY=results[0].pose.nose.y+20;
+     console.log("nose x="+noseX);
+     console.log("nose y="+noseY);
     }
 }
 
 function draw(){
     image(video,0,0,300,300);
+    image(lipstick,noseX,noseY,50,20);
 }
 
 function take_snapshot(){
